29ecbe5c4d5303f6af35a6f117f1dec337001654
PrometeusRec <98043953+PrometeusRec@users.noreply.github.com>
2022-05-15 11:31:31 -0400

    Add files via upload

---
+++ mORMot_JSON_Test.dpr
@@ -0,0 +1,62 @@
+program mORMot_JSON_Test;
+
+{$APPTYPE CONSOLE}
+
+uses
+  mormot.core.os, mormot.core.json;
+
+type TTestArray    = array of packed record
+                                       Record01     : packed record
+                                                               ax   : TDateTime;
+                                                               bx   : String;
+                                                               cx   : Double;
+                                                               dx   : Int64;
+                                                               ex   : array of String;
+                                                               fx   : Int64;
+                                                               gx   : String;
+                                                             end;
+                                       Record02     : packed record
+                                                               az   : TDateTime;
+                                                               bz   : String;
+                                                               cz   : Double;
+                                                               &as  : Int64;
+                                                               ez   : String;
+                                                               fz   : Double;
+                                                               gz   : Int64;
+                                                               hz   : array of String;
+                                                             end;
+                                       Record03     : packed record
+                                                               ad   : TDateTime;
+                                                               bd   : Double;
+                                                               cd   : Double;
+                                                               dd   : Double;
+                                                               ed   : Double;
+                                                               fd   : Int64;
+                                                             end;
+                                       Record04     : packed record
+                                                               ad   : TDateTime;
+                                                               bd   : Double;
+                                                               cd   : Double;
+                                                               dd   : Double;
+                                                               ed   : Double;
+                                                               fd   : Int64;
+                                                             end;
+                                       Record05     : packed record
+                                                               ad   : TDateTime;
+                                                               bd   : Double;
+                                                               cd   : Double;
+                                                               dd   : Double;
+                                                               ed   : Double;
+                                                               fd   : Int64;
+                                                             end;
+                                     end;
+
+var tmp       : String;
+    new_array : TTestArray;
+
+
+begin
+  tmp := StringFromFile('JSON_Test.TXT');
+  DynArrayLoadJson(new_array,Pointer(tmp),TypeInfo(TTestArray));
+//  RecordLoadJson(new_array,Pointer(tmp),TypeInfo(TTestArray));
+end.
