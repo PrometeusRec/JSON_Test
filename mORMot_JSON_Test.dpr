19489bdf1d01f5f90673dbe0b8da29601a63e71b
PrometeusRec <98043953+PrometeusRec@users.noreply.github.com>
2023-01-02 19:30:46 -0500

    Update mORMot_JSON_Test.dpr

--- mORMot_JSON_Test.dpr
+++ mORMot_JSON_Test.dpr
@@ -5,57 +5,58 @@ program mORMot_JSON_Test;
 uses
   mormot.core.os, mormot.core.json;
 
-type TTestArray    = array of packed record
-                                       symbol       : String;
-                                       Record01     : packed record
-                                                               ax   : TDateTime;
-                                                               bx   : String;
-                                                               cx   : Double;
-                                                               dx   : Int64;
-                                                               ex   : array of String;
-                                                               fx   : Int64;
-                                                               gx   : String;
-                                                             end;
-                                       Record02     : packed record
-                                                               az   : TDateTime;
-                                                               bz   : String;
-                                                               cz   : Double;
-                                                               &as  : Int64;
-                                                               ez   : String;
-                                                               fz   : Double;
-                                                               gz   : Int64;
-                                                               hz   : array of String;
-                                                             end;
-                                       Record03     : packed record
-                                                               ad   : TDateTime;
-                                                               bd   : Double;
-                                                               cd   : Double;
-                                                               dd   : Double;
-                                                               ed   : Double;
-                                                               fd   : Int64;
-                                                             end;
-                                       Record04     : packed record
-                                                               ad   : TDateTime;
-                                                               bd   : Double;
-                                                               cd   : Double;
-                                                               dd   : Double;
-                                                               ed   : Double;
-                                                               fd   : Int64;
-                                                             end;
-                                       Record05     : packed record
-                                                               ad   : TDateTime;
-                                                               bd   : Double;
-                                                               cd   : Double;
-                                                               dd   : Double;
-                                                               ed   : Double;
-                                                               fd   : Int64;
-                                                             end;
-                                     end;
+type TTest    = packed record
+                         city         : String;
+                         record01     : packed record
+                                                 ax   : TDateTime;
+                                                 bx   : String;
+                                                 cx   : Double;
+                                                 dx   : Int64;
+                                                 ex   : array of String;
+                                                 fx   : Int64;
+                                                 gx   : String;
+                                               end;
+                         record02     : packed record
+                                                 az   : TDateTime;
+                                                 bz   : String;
+                                                 cz   : Double;
+                                                 &as  : Int64;
+                                                 ez   : String;
+                                                 fz   : Double;
+                                                 gz   : Int64;
+                                                 hz   : array of String;
+                                               end;
+                         record03     : packed record
+                                                 ad   : TDateTime;
+                                                 bd   : Double;
+                                                 cd   : Double;
+                                                 dd   : Double;
+                                                 ed   : Double;
+                                                 fd   : Int64;
+                                               end;
+                         record04     : packed record
+                                                 ad   : TDateTime;
+                                                 bd   : Double;
+                                                 cd   : Double;
+                                                 dd   : Double;
+                                                 ed   : Double;
+                                                 fd   : Int64;
+                                               end;
+                         record05     : packed record
+                                                 ad   : TDateTime;
+                                                 bd   : Double;
+                                                 cd   : Double;
+                                                 dd   : Double;
+                                                 ed   : Double;
+                                                 fd   : Int64;
+                                               end;
+                       end;
+    TTestArray = array of TTest;
 
-var tmp       : String;
+var tmp       : RawJson;
     new_array : TTestArray;
 
 begin
   tmp := StringFromFile('JSON_Test_03.TXT');
-  DynArrayLoadJson(new_array,Pointer(tmp),TypeInfo(TTestArray));
+  DynArrayLoadJson(new_array, Pointer(tmp), TypeInfo(TTestArray));
 end.
